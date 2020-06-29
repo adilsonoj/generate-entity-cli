25a53a8f66a3e3f0963fa358070c5a5561235ebe
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-06-29 15:53:49 -0300

    fix: DataSourceBuilder<?>

--- src/main/java/dev/oliver/cli/generateentity/config/DataSourceConfig.java
+++ src/main/java/dev/oliver/cli/generateentity/config/DataSourceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 public class DataSourceConfig {
 
     public static DataSource getDataSource(String username, String password, String server, String nameSpace) {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("oracle.jdbc.OracleDriver");
         dataSourceBuilder.url("jdbc:oracle:thin:@" + server + ":1521:" + nameSpace + "");
         dataSourceBuilder.username(username);
